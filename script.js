071894e102a612c67cf8ef9a2145bb45d2cfea55
Greg Johnston <gregmj.sco@gmail.com>
2023-08-04 13:04:05 +0900

    First implementation of exercise task from objects chapter

---
+++ script.js
@@ -0,0 +1,15 @@
+function Book (title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+Book.prototype.showInfo = function() {
+    let info = `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+    return info;
+}
+
+const testBook = new Book('The Win', 'Misa', 342, 'is read');
+
+console.log(testBook.showInfo());
